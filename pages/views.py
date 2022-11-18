6b3c686ec4256f69ba25c527e91b8c12fe11aecc
АД <degterevastasa@gmail.com>
2022-11-18 15:30:34 +0300

    pages

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 
-from hospital.models import MedicalTestEntity, CureEntity
+from hospital.models import MedicalTestEntity, CureEntity, UserEntity
 
 
 def home(request, *args, **kwargs):
@@ -13,9 +14,6 @@ def register_staff(request, *args, **kwargs):
 
 
 def add_stuff(request, *args, **kwargs):
-    d = request.POST.items()
-    for i, val in d:
-        print(i, val)
     return HttpResponse("Hello")
 
 
@@ -30,12 +28,20 @@ def add(request, *args, **kwargs):
     return HttpResponse("Hello")
 
 
-def sign_in(request, *args, **kwargs):
-    return render(request, 'sign_in.html', {})
+def log_in_clt(request, *args, **kwargs):
+    return render(request, 'log_in_clt.html', {})
+
+
+def log_in_stf(request, *args, **kwargs):
+    return render(request, 'log_in_stf.html', {})
+
+
+def log_in_adm(request, *args, **kwargs):
+    return render(request, 'log_in_admin.html', {})
 
 
 def register_card(request, *args, **kwargs):
-    return render(request, 'register_card.html', {})
+    return render(request, 'client_register_card.html', {})
 
 
 def choose_med_test(request, *args, **kwargs):
@@ -43,7 +49,7 @@ def choose_med_test(request, *args, **kwargs):
     return render(request, 'medical_test.html', {"med_tests": med_tests})
 
 
-def choose_cure(request, *args, **kwargs):
+def find_cure(request, *args, **kwargs):
     title = request.GET.get('cures_search')
     try:
         cures = CureEntity.objects.filter(title=title)
@@ -52,3 +58,28 @@ def choose_cure(request, *args, **kwargs):
     else:
         content = {"cures": cures}
     return render(request, 'cure.html', content)
+
+
+def client_homepage(request, *args, **kwargs):
+    return render(request, 'client_home.html', {})
+
+
+def staff_homepage(request, *args, **kwargs):
+    return render(request, 'staff_home.html', {})
+
+
+def card_registration_list(request, *args, **kwargs):
+    return render(request, "doctor_register_card.html", {})
+
+
+def client_list(request, *args, **kwargs):
+    return render(request, 'doctor_clients.html', {})
+
+
+def admin_hp(request, *args, **kwargs):
+    return render(request, 'admin_hm.html', {})
+
+
+def adm_register_staff(request, *args, **kwargs):
+    return render(request, "admin_register_staff.html", {})
+
