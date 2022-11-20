7e8b75e3c60db37ca89ea9e79a9e3b5cd1132c50
АД <degterevastasa@gmail.com>
2022-11-20 14:23:16 +0300

    final version of data classes

--- hospital/models.py
+++ hospital/models.py
@@ -208,7 +208,15 @@ class Constants(models.Model):
     hospital_sections = models.CharField(max_length=40)
     hospital_info = models.TextField()
 
+#     dc
+    dc_id = models.AutoField(primary_key=True)
+    doctor_speciality = models.CharField(max_length=50)  # should be FK
+    cabinet_no = models.CharField(max_length=4)  # redundant. doctor has cabinet
+    date = models.CharField(max_length=12)
+    time = models.CharField(max_length=4)
 
+#     med test
+    met_test_id = models.AutoField(primary_key=True)
 
 
 
